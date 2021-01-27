a93d9b946a5301d8924cdb15adf8f5cee6013782
aldidestan <destaodervon@gmail.com>
2021-01-27 13:23:36 +0700

    User Client: 1. update layout event detail 2. add selector ticket 3. add capacity info 4. update button

--- user/src/component/CardEventDetail.jsx
+++ user/src/component/CardEventDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Container, Card, CardGroup, Form, Row, Col, Button, Modal } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom' 
+import { useHistory } from 'react-router-dom'
 
 export default function CardEventDetail(props) {
   const history = useHistory();
@@ -12,78 +12,38 @@ export default function CardEventDetail(props) {
     history.push('/history');
   }
 
-  if(!props.ticket) {
+  if (!props.ticket) {
     return <h1>Loading ...</h1>
   }
 
+  const goHome = () => {
+    history.push('/')
+  }
+  {/*dummy capacity seat dan harga ticket */ }
+  const jumlahCapacity = 0
+  const priceTicket = true
+  const regTicket = 100
+  const vipTicket = 1000
+  const wipTicket = 10000
+  const statusTicket = 'unpaid'
+
   return (
     <Container fluid>
-      <div>{JSON.stringify(props.ticket)}</div>
-      <Button
-            onClick={goHistory}
-            style={{
-              width: 100,
-              height: 30,
-              color: 'whitesmoke',
-              backgroundColor: '#F2C94C',
-              border: 'none'
-            }}
-          >
-            Back to History
-          </Button>
-          
-      {/* <Modal
-        size="lg"
-        show={show}
-        onHide={handleClose}
-        aria-labelledby="example-modal-sizes-title-lg"
+      {/* <div>{JSON.stringify(props.ticket)}</div> */}
+      {/* <Button
+        onClick={goHistory}
+        style={{
+          width: 100,
+          height: 30,
+          color: 'whitesmoke',
+          backgroundColor: '#F2C94C',
+          border: 'none'
+        }}
       >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-modal-sizes-title-lg">
-            Ticket
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId='email'>
-              <Form.Label><strong>Email</strong></Form.Label>
-              <Form.Control type='email' placeholder='asd' />
-            </Form.Group>
-            <Form.Group controlId='password'>
-              <Form.Label><strong>Password</strong></Form.Label>
-              <Form.Control type='password' placeholder='asd' />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            onClick={submitHandle}
-            style={{
-              width: 100,
-              height: 30,
-              color: 'whitesmoke',
-              backgroundColor: '#F2C94C',
-              border: 'none'
-            }}
-          >
-            Submit
-          </Button>
-          <Button
-            onClick={cancelHandle}
-            style={{
-              width: 100,
-              height: 30,
-              color: 'whitesmoke',
-              backgroundColor: '#F2C94C',
-              border: 'none'
-            }}
-          >
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal> */}
+        Back to History
+          </Button> */}
 
-      {/* <CardGroup className="m-4 d-block">
+      <CardGroup className="m-4 d-block">
         <Card className="m-5 border-0 shadow" style={{
           backgroundColor: '#FFF5D5',
           borderRadius: 10,
@@ -101,41 +61,55 @@ export default function CardEventDetail(props) {
               <Card.Body>
                 <Form>
                   <Form.Group controlId='formGroupEventName'>
-                    <Form.Label><strong>Email</strong></Form.Label>
-                    <Form.Control type='text' placeholder='user@mail.com' disabled />
+                    <Form.Label><strong>Event Name</strong></Form.Label>
+                    <Form.Control type='text' placeholder={'Weeding'} disabled />
                   </Form.Group>
                   <Form.Group controlId='formGroupEventOrganizer'>
                     <Form.Label><strong>Event Organizer</strong></Form.Label>
-                    <Form.Control type='text' placeholder='Bromo Ranger' disabled />
+                    <Form.Control type='text' placeholder={'Bromo Ranger'} disabled />
                   </Form.Group>
-                  <Form.Group controlId='formGroupVenue'>
-                    <Form.Label><strong>Password</strong></Form.Label>
-                    <Form.Control type='text' placeholder='Bromo' disabled />
+                  <Form.Group controlId='formGroupLocation'>
+                    <Form.Label><strong>Location</strong></Form.Label>
+                    <Form.Control type='text' placeholder={'Bromo'} disabled />
                   </Form.Group>
                   <Row>
                     <Col>
                       <Form.Group controlId='formGroupEventDate'>
                         <Form.Label><strong>Event Date</strong></Form.Label>
-                        <Form.Control type='text' placeholder='12-05-2021' disabled />
+                        <Form.Control type='text' placeholder={'12-05-2021'} disabled />
                       </Form.Group>
                     </Col>
                     <Col>
-                      <Form.Group controlId='formGroupPricee'>
-                        <Form.Label><strong>Price</strong></Form.Label>
-                        <Form.Control type='text' placeholder='Rp 150.000' disabled />
+                      <Form.Group controlId='formGroupTime'>
+                        <Form.Label><strong>Event Time</strong></Form.Label>
+                        <Form.Control type='text' placeholder={'19:30'} disabled />
                       </Form.Group>
                     </Col>
                   </Row>
-                  <Form.Group controlId='formGroupName'>
-                    <Form.Label><strong>Name</strong></Form.Label>
-                    <Form.Control type='text' placeholder='Name' />
-                  </Form.Group>
+                  {(!priceTicket) ?
+                    <div></div> :
+                    <Form.Group controlId='formGroupName'>
+                      <Form.Label><strong>Ticket Class</strong></Form.Label>
+                      <Form.Control as="select" defaultValue="Choose...">
+                        <option> - Choose Ticket - </option>
+                        <option>{'Reguler'}:  {regTicket}</option>
+                        <option>{'VIP'}:  {vipTicket}</option>
+                        <option>{'WIP Ticket'}:  {wipTicket}</option>
+                      </Form.Control>
+                    </Form.Group>
+                  }
+                  {(jumlahCapacity <= 0) ?
+                    <div></div> :
+                    <span>Jumlah: {jumlahCapacity} Capacity</span>
+                  }
                   <br />
                 </Form>
+                {(statusTicket === 'unpaid') ?
                   <Button
                     // onClick={handleShow}
                     type='submit'
                     style={{
+                      marginRight: 5,
                       width: 100,
                       height: 30,
                       color: 'whitesmoke',
@@ -143,13 +117,41 @@ export default function CardEventDetail(props) {
                       border: 'none'
                     }}
                   >
-                    Choose
+                    Pay Now
+                  </Button> :
+                  <Button
+                    // onClick={handleShow}
+                    type='submit'
+                    style={{
+                      margin: 5,
+                      width: 100,
+                      height: 30,
+                      color: 'whitesmoke',
+                      backgroundColor: '#F2C94C',
+                      border: 'none'
+                    }}
+                  >
+                    Book
                   </Button>
+                }
+                <Button
+                  onClick={goHome}
+                  style={{
+                    margin: 5,
+                    width: 100,
+                    height: 30,
+                    color: 'whitesmoke',
+                    backgroundColor: '#F2C94C',
+                    border: 'none'
+                  }}
+                >
+                  Cancel
+                </Button>
               </Card.Body>
             </Col>
           </Row>
         </Card>
-      </CardGroup> */}
+      </CardGroup>
     </Container>
   )
 }
